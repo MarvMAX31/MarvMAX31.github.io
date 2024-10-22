f9cf60be1fa54d6bce5e85959e30a936edf82619
ahmadfauzi <126822580+MarvMAX31@users.noreply.github.com>
2024-10-22 15:52:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+// Fungsi untuk mengaktifkan tombol pesan sekarang
+function pesanSekarang() {
+    alert("Pesan berhasil!");
+}
+
+// Fungsi untuk mengaktifkan tombol beli sekarang
+function beliSekarang() {
+    alert
